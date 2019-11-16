f11693acfc512f633fc91c7af2f031e06e209dd0
Yuriy <ivanivyura@gmail.com>
2019-11-16 22:36:39 +0200

    refactor done

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,30 +1,57 @@
 import React from 'react';
 
-const filterButtons = [
-  { name: 'all', label: 'All' },
-  { name: 'active', label: 'Active' },
-  { name: 'done', label: 'Done' }
-];
 
-const ItemStatusFilter = ({filter, onFilterChange = () => {}}) => {
 
-  const buttons = filterButtons.map(({name, label}) => {
+const ItemStatusFilter = ({ filter, onFilterChange = () => { } }) => {
+
+  const buttons = filterButtons.map(({ name, label }) => {
     const isActive = name === filter;
     const classNames = 'btn ' + (isActive ? 'btn-info' : 'btn-outline-secondary');
 
     return (
       <button key={name}
-              type="button"
-              onClick={() => onFilterChange(name)}
-              className={classNames}>{label}</button>
+        type="button"
+        onClick={() => onFilterChange(name)}
+        className={classNames}>{label}
+      </button>
     );
   });
 
   return (
     <div className="btn-group">
-      { buttons }
+      {buttons}
     </div>
   );
 };
 
 export default ItemStatusFilter;
+
+
+export default class ItemStatusFilter extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  filterButtons = [
+    { name: 'all', label: 'All' },
+    { name: 'active', label: 'Active' },
+    { name: 'done', label: 'Done' }
+  ];
+
+  render() {
+    return <div>123</div>
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
